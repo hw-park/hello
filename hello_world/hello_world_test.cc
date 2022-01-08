f7a7a1e78a843324c7c1dde17db95f5e5851d0a8
hw-park <hw-park@users.noreply.github.com>
2022-01-08 23:44:14 +0900

    Update comments.

--- hello_world/hello_world_test.cc
+++ hello_world/hello_world_test.cc
@@ -9,5 +9,10 @@ TEST(HelloWorldTest, GetHelloWorldStringWorks) {
   EXPECT_EQ(getHelloWorldString(), "Hello, World.");
 }
 
+<<<<<<< HEAD
 }
 }  // namespace hello_world
+=======
+} // namespace
+} // namespace hello_world
+>>>>>>> 2397928 (Update comments.)
