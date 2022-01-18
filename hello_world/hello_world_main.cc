46203cce2223eb7b4410f8293f3581b53a61bfdd
Haewoo Park <starlet@Darling.kornet>
2022-01-19 00:35:13 +0900

    Set up logging properly.

--- hello_world/hello_world_main.cc
+++ hello_world/hello_world_main.cc
@@ -1,11 +1,13 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "gflags/gflags.h"
 #include "glog/logging.h"
 #include "hello_world/hello_world.h"
 
 int main(int argc, char** argv) {
   ::google::InitGoogleLogging(argv[0]);
+  ::google::ParseCommandLineFlags(&argc, &argv, true);
 
   std::cout << ::hello_world::getHelloWorldString() << std::endl;
 
